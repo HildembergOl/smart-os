e8ab980ac9d0acccdee1cb1c1b166d32d989cca8
Hildemberg Oliveira <hildembergc.oliveira@gmail.com>
2024-02-15 11:06:33 -0300

    Refactoring: Ajus base api for axios to production.

--- lib/api.tsx
+++ lib/api.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import axios from 'axios'
 // import Cookies from 'js-cookie'
 // const token = Cookies.get('tokenUserWMS')
@@ -6,7 +5,8 @@ import axios from 'axios'
 
 const api = axios.create()
 
-api.defaults.baseURL = 'http://localhost:3000/api/'
+api.defaults.baseURL = '/api/'
 api.defaults.headers['Access-Control-Allow-Origin'] = '*'
+api.defaults.timeout = 10000
 
 export default api
